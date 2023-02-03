221d40dd29a382bcd629c7b8f19a440985f61ce6
Beytullah Tuncer <tuncerbeytullah6464@gmail.com>
2023-02-03 19:02:25 +0300

    ahmet

--- src/test/java/tests/US07/Test01.java
+++ src/test/java/tests/US07/Test01.java
@@ -5,5 +5,9 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("deneme github");
         System.out.println("deneme github");
+        System.out.println("deneme github");
+        System.out.println("deneme github");
+        System.out.println("deneme github");
+        System.out.println("deneme github");
     }
 }
